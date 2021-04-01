1cbaa1692160ca48d69b43047898ac266acc1c3f
LinGuanMing <solitaryseal@gmail.com>
2021-04-01 10:24:23 +0800

    Delete DateTranslate directory

--- DateTranslate/DateTranslate.cs
+++
@@ -1,6 +0,0 @@
-string DATE = "1061010";
-
-string new_date = Convert.ToString(Convert.ToInt32(DATE) + 19110000);
-IFormatProvider culture = new System.Globalization.CultureInfo("zh-TW", true);
-DateTime NewDate = DateTime.ParseExact(new_date, "yyyyMMdd", culture);
-string DATE_ADD90 = Convert.ToString(Convert.ToInt32(NewDate.AddDays(89).ToString("yyyyMMdd")) - 19110000);
